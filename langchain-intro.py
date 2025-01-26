e339a939a7ba133cc7b2cbe16041e31444b15190
Kevin Kakolla <nageswarkv@gmail.com>
2025-01-26 00:04:16 -0800

    remove open api key

--- langchain-intro.py
+++ langchain-intro.py
@@ -5,21 +5,30 @@ import os
 import getpass
 from langchain.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
+from langchain.chains.sequential import  SequentialChain
 
-os.environ["OPENAI_API_KEY"]= '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'
+os.environ["OPENAI_API_KEY"]= ''
 print('Hello LangChain')
 
 llm = OpenAI()
 code_prompt = PromptTemplate(template='write a very {language} function that will complete {task}',input_variables=["language","task"])
 
+test_prompt = PromptTemplate(template='write a test using Pytest {language} for {code}',input_variables=["language","code"])
 
-code_chain = LLMChain(llm=llm,prompt = code_prompt)
+code_chain = LLMChain(llm=llm,prompt = code_prompt,output_key="code")
+test_chain = LLMChain(llm=llm,prompt=test_prompt,output_key = "test")
 lang = input('what\'s ur language')
 task = input('whats ur task')
-result = code_chain({
+
+chain = SequentialChain(
+    chains=[code_chain,test_chain],
+    input_variables = ["language","task"],
+    output_variables = ["test","code"]
+)
+result = chain({
     "language":lang,
     "task":task
 })
-print(result["text"])
+print(result['test'])
 
 
